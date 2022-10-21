d36a19befa9dd03b6fda4061901e85d6f5256622
Dongjae <gyeheon1234@gmail.com>
2022-10-21 15:12:45 +0900

    Base

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
-from flask import Flask
+from flask import Flask, request, render_template
+from datetime import datetime
+import re
+
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello World"
+    return render_template('main.html')
+
+@app.route("/<BoothName>")
+def booth():
+    return render_template(f'{booth}.html')
+
+
+if __name__ == '__main__':
+    app.run()
