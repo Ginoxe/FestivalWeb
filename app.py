cfe9fae9fbd7cd7f74af9713ae8e661e3884bf5d
Dongjae Park <derpykraken0421@gmail.com>
2022-10-25 06:50:01 +0900

    just little work

--- app.py
+++ app.py
@@ -8,10 +8,29 @@ app = Flask(__name__)
 def home():
     return render_template('main.html')
 
-@app.route("/<BoothName>")
-def booth():
+@app.route("/<season>")
+def section(season):
+    return render_template(f'{season}.html')
+
+@app.route("/Spring/<booth>")
+def booth(booth):
     return render_template(f'{booth}.html')
 
+@app.route("/Summer/<booth>")
+def booth(booth):
+    return render_template(f'{booth}.html')
+
+@app.route("/Autumn/<booth>")
+def booth(booth):
+    return render_template(f'{booth}.html')
+
+@app.route("/Winter/<booth>")
+def booth(booth):
+    return render_template(f'{booth}.html')
+
+@app.route("/Picnic/<booth>")
+def booth(booth):
+    return render_template(f'{booth}.html')
 
 if __name__ == '__main__':
     app.run()
