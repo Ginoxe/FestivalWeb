e468a920b770e5307e4b6ced1f11f7744961c568
Dongjae Park <derpykraken0421@gmail.com>
2022-10-26 06:58:17 +0900

    :sd

--- app.py
+++ app.py
@@ -8,29 +8,45 @@ app = Flask(__name__)
 def home():
     return render_template('main.html')
 
-@app.route("/<season>")
-def section(season):
-    return render_template(f'{season}.html')
+@app.route("/Spring")
+def spring():
+    return render_template('Spring.html')
+
+@app.route("/Summer")
+def summer():
+    return render_template('Summer.html')
+
+@app.route("/Autumn")
+def autumn():
+    return render_template('Autumn.html')
+
+@app.route("/Winter")
+def winter():
+    return render_template('Winter.html')
+
+@app.route("/Picnic")
+def picnic():
+    return render_template('Picnic.html')
 
 @app.route("/Spring/<booth>")
-def Spring(booth):
+def SpringBooth(booth):
     return render_template(f'Spring{booth}.html')
 
 @app.route("/Summer/<booth>")
-def Summer(booth):
+def SummerBooth(booth):
     return render_template(f'Summer{booth}.html')
 
 @app.route("/Autumn/<booth>")
-def Autumn(booth):
+def AutumnBooth(booth):
     return render_template(f'Autumn{booth}.html')
 
 @app.route("/Winter/<booth>")
-def Winter(booth):
+def WinterBooth(booth):
     return render_template(f'Winter{booth}.html')
 
 @app.route("/Picnic/<booth>")
-def booth(booth):
-    return render_template(f'{booth}.html')
+def PicnicBooth(booth):
+    return render_template(f'Picnic{booth}.html')
 
 if __name__ == '__main__':
     app.run()
